38e1a7937207a71a77d206232c3e342e9ad1569b
gareins <ozbolt.menegatti@gmail.com>
2013-02-13 11:53:40 +0100

    FirstCommit

---
+++ Source/Moron/Movie.py
@@ -0,0 +1,19 @@
+'''
+Created on Feb 13, 2013
+
+@author: ozbolt
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
+if __name__ == "__main__":
+    print("mjau")
